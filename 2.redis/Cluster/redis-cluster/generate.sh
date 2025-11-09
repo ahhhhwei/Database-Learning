207d7661ebea89e7c9ebed4578811d56858bda86
ahwei <2084804889@qq.com>
2025-11-09 15:53:23 +0800

    cluster code

---
+++ 2.redis/Cluster/redis-cluster/generate.sh
@@ -0,0 +1,35 @@
+# 生成 redis1 ~ redis9 的配置
+for port in $(seq 1 9); do
+  mkdir -p redis${port}/
+  cat << EOF > redis${port}/redis.conf
+port 6379
+bind 0.0.0.0
+protected-mode no
+appendonly yes
+cluster-enabled yes
+cluster-config-file nodes.conf
+cluster-node-timeout 5000
+cluster-announce-ip 172.30.0.10${port}
+cluster-announce-port 6379
+cluster-announce-bus-port 16379
+EOF
+done
+
+
+# 生成 redis10 ~ redis11 的配置
+for port in $(seq 10 11); do
+  mkdir -p redis${port}/
+  cat << EOF > redis${port}/redis.conf
+port 6379
+bind 0.0.0.0
+protected-mode no
+appendonly yes
+cluster-enabled yes
+cluster-config-file nodes.conf
+cluster-node-timeout 5000
+cluster-announce-ip 172.30.0.1${port}
+cluster-announce-port 6379
+cluster-announce-bus-port 16379
+EOF
+done
+
